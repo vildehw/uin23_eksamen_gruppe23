da0106061536e8f400d806de04282fbc182a7a85
Synnehe <synnehei@hotmail.no>
2023-04-24 12:23:04 +0200

    laget en frontend mappe

--- frontend/src/components/GameCard.js
+++ frontend/src/components/GameCard.js
@@ -1,11 +1,14 @@
 export default function GameCard({games}) {
+
+  console.log(games)
   return(
-    <>
+    <section>
         {games?.map((game, index) => (
-        <article key={index}>
+        <article key={index} className="gamecard">
         <img src={game.background_image} alt={game.name}/>
+        <h3>{game.name}</h3>
         </article>
       ))}
-    </>
+    </section>
   )
 }
