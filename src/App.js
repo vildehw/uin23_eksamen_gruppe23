283ef2360399087cb16a69f053b8c1058b8ef153
Synnehe <synnehei@hotmail.no>
2023-04-24 11:06:43 +0200

    hentet API

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './App.css'; 
 import Dashboard from './components/Dashboard';
 import GameShop from './components/GameShop';
@@ -7,6 +8,20 @@ import './css/main.css'
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
+
+  const getGames = async() => {
+    const response = await fetch (`https://api.rawg.io/api/platforms?key=880241c0a7e24864aef2b9d1687af70d`)
+    const data = await response.json()
+    console.log(data)
+  }
+
+  useEffect(() =>{
+    getGames()
+  },[])
+
+
+
+
   return (
     <Routes>
       <Route path='/' element={<Dashboard />}/>
