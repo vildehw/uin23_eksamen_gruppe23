154ed818faa64d5906e2756c2a51e80f95a4bbb3
Synnehe <synnehei@hotmail.no>
2023-04-29 17:24:34 +0200

    Endret på dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,10 +1,40 @@
 import GameCard from "./GameCard";
 
 export default function Dashboard({games}) {
-
   return(
     <>
-      <GameCard games={games} />
+      <div className="section-headline">
+        <h2>GameShop</h2>
+        <button>Visit shop</button>
+      </div>
+      <section id="gameshop-window">
+        <GameCard />
+        <GameCard />
+        <GameCard />
+      </section>
+      <section id="my-games-section">
+        <section>
+          <div className="section-headline">
+          <h2>MY GAMES-LIBRARY - 21 games</h2>
+          </div>
+          <article className="gamecard">
+            <div className="poster">
+            </div>
+            <div className="game-info">
+              <h3>Title</h3>
+              <h4>Genre</h4>
+              <p>Hours played: 96</p>
+            </div>
+            <button>Go to library</button>
+          </article>
+        </section>
+        <section id="favourites-section">
+          <div className="section-headline">
+          <h2>MY FAVOURITES</h2>
+          </div>
+          <button>Go to favourites</button>
+        </section>
+      </section>
     </>
   )
 }
