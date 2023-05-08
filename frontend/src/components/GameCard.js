6627d3838a34d967b91d4779d9353faa69d42cd1
Synnehe <synnehei@hotmail.no>
2023-05-08 10:28:09 +0200

    oppdatert css

--- frontend/src/components/GameCard.js
+++ frontend/src/components/GameCard.js
@@ -2,8 +2,7 @@ export default function GameCard({games, gamesAmount}) {
   return(
   games?.filter((game, index) => (index < gamesAmount)).map((game, index) => (
     <article className="gamecard" key={index}>
-    <div className="poster">
-    </div>
+    <img src={game.background_image} alt={game.name + " poster"} className="poster" />
     <div className="game-info">
       <h3>{game.name}</h3>
       <h4>Genre</h4>
