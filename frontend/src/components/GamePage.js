c2ab296afd1df51b3950d0e547c4338e54e931f3
vildehw <vildehw@hiof.no>
2023-05-08 16:22:41 +0200

    henter ut game info

--- frontend/src/components/GamePage.js
+++ frontend/src/components/GamePage.js
@@ -27,13 +27,30 @@ export default function GamePage({games}) {
   return(
     <> 
     <h1>{selectedGame?.name}</h1>  
-    
-
-      <p>Plot: {gameInfo?.description_raw}</p>
-   
-    <img></img> 
-    <p>rating: {selectedGame?.rating}</p>
-
+    <img src={gameInfo?.background_image} alt={selectedGame?.name}></img> 
+    <p>Rating: {gameInfo?.rating}</p>   
+    <p>Genre: <ul>{gameInfo?.genres.map((g,i) => <li>{g.name}</li>)}</ul></p>
+    <p>Plot: {gameInfo?.description_raw}</p> 
+    <p>Tags:</p> <ul>{gameInfo?.tags.map((t,i) => <li>{t.name}</li>)}</ul> 
+    <p>Developers:</p> <ul>{gameInfo?.developers.map((d,i) => <li>{d.name}</li>)}</ul> 
+    <p>Publisher:</p> <ul>{gameInfo?.publishers.map((p,i) => <li>{p.name}</li>)}</ul> 
+    <p>Release: {gameInfo?.released}</p> 
+    <p>Platforms:</p><ul>{gameInfo?.platforms.map((p,i) => <li>{p.platform.name}</li>)}</ul>  
+    <p>Stores:</p> <ul>{gameInfo?.stores.map((s,i) => <li>{s.store.name}</li>)}</ul> 
+    <button>add to favorites</button>
     </>
   )
-}
+} 
+
+/* 
+Bilde
+Rating
+Oppsummering/plot
+Stikkord (tags)
+Utviklere (developers)
+Utgiver (publisher)
+Utgivelsesår (release)
+Plattformer (platforms)
+Kjøpsmuligheter (stores)  
+
+*/
