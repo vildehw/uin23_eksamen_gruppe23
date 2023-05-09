35ecee9cde349037105b7f8279368427d243527d
vildehw <vildehw@hiof.no>
2023-05-09 14:41:51 +0200

    henter mer data fra api til hvert spill

--- frontend/src/components/GamePage.js
+++ frontend/src/components/GamePage.js
@@ -1,31 +1,34 @@
-<<<<<<< HEAD
-import { add } from "lodash"
-import { useEffect, useState } from "react"
-=======
->>>>>>> 044ac03ed514b0b58a62604927af4b6bdd8e53c5
 import { useParams } from "react-router"
 import { fetchGame } from "../sanity/gameServices"
 import { useEffect, useState } from "react"
 
-export default function GamePage() {
+export default function GamePage({games}) {
  
   const {slug} = useParams()
 
-  const [sanityGame, setSanityGame] = useState(null) 
+ 
 
-  const getsanityGame = async (slug) => {
-    const data = await fetchGame(slug) 
-    setSanityGame(data[0])
-  }  
+  ///////////////  
 
-  useEffect(() => {
-    getsanityGame(slug)
-  }, [slug]) 
+const selectedGame = games?.find((game) => game?.slug === slug) 
+const id = selectedGame?.id
 
-<<<<<<< HEAD
+const [gameInfo, setGameInfo] = useState([]) 
+
+
+  const getGameInfo = async() => {
+    const response = await fetch (`https://api.rawg.io/api/games/${id}?key=880241c0a7e24864aef2b9d1687af70d`)
+    const data = await response.json()
+    setGameInfo(data) 
+    console.log(gameInfo)
+  }
+
   useEffect(() =>{
     getGameInfo()
   },[selectedGame])  
+
+  console.log(games)
+  console.log(selectedGame) 
 
 //kode for lage favoritt  
 
@@ -48,32 +51,22 @@ export default function GamePage() {
 console.log(favourites)
 
   return(
-    <> 
+    <>  
+    
     <h1>{selectedGame?.name}</h1>  
     <img src={gameInfo?.background_image} alt={selectedGame?.name}></img> 
     <p>Rating: {gameInfo?.rating}</p>   
     <p>Plot: {gameInfo?.description_raw}</p> 
-    <p>Tags:</p> <ul>{gameInfo?.tags.map((t,i) => <li>{t.name}</li>)}</ul> 
-    <p>Developers:</p> <ul>{gameInfo?.developers.map((d,i) => <li>{d.name}</li>)}</ul> 
-    <p>Publisher:</p> <ul>{gameInfo?.publishers.map((p,i) => <li>{p.name}</li>)}</ul> 
+    <p>Tags:</p> <ul>{gameInfo?.tags?.map((t,i) => <li>{t.name}</li>)}</ul> 
+    <p>Developers:</p> <ul>{gameInfo?.developers?.map((d,i) => <li>{d.name}</li>)}</ul> 
+    <p>Publisher:</p> <ul>{gameInfo?.publishers?.map((p,i) => <li>{p.name}</li>)}</ul> 
     <p>Release: {gameInfo?.released}</p> 
-    <p>Platforms:</p><ul>{gameInfo?.platforms.map((p,i) => <li>{p.platform.name}</li>)}</ul>  
-    <p>Stores:</p> <ul>{gameInfo?.stores.map((s,i) => <li>{s.store.name}</li>)}</ul> 
+    <p>Platforms:</p><ul>{gameInfo?.platforms?.map((p,i) => <li>{p.platform.name}</li>)}</ul>  
+    <p>Stores:</p> <ul>{gameInfo?.stores?.map((s,i) => <li>{s.store.name}</li>)}</ul> 
     <button onClick={addFavourite}>add to favorites</button>
+    
+
+    
     </>
   )
 } 
-
-=======
-  console.log(sanityGame)
-
-    return( 
-        <>
-        <h2>{sanityGame?.game_title} </h2> 
-        <p>Played: {sanityGame?.playtime} hours</p>    
-        <p>Genre: {sanityGame?.genre}</p>
-        
-        </>
-    )
-}
->>>>>>> 044ac03ed514b0b58a62604927af4b6bdd8e53c5
